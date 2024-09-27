2d43daf465a53c505fad15e90df7d8fcff6d9db7
Garth Johnson <growlf@thenetyeti.com>
2024-09-26 19:15:51 -0700

    initial import

---
+++ src/DFMultiEnvironmental.cpp
@@ -0,0 +1,31 @@
+#include "DFMultiEnvironmental.h"
+
+/// @brief Creates a new multi environmental sensor object
+/// @param I2C_bus The I2C bus attached to the sensor
+/// @param address Address of the sensor
+DFMultiEnvironmental::DFMultiEnvironmental(TwoWire* I2C_bus, uint8_t address) : env_sensor(address, I2C_bus) {}
+
+/// @brief Starts the enviromental sensor
+/// @return True on success
+bool DFMultiEnvironmental::begin() {
+	values.resize(6);
+	Description.parameterQuantity = 6;
+	Description.type = "Multi Environment Sensor";
+	Description.name = "Environmental Sensor";
+	Description.parameters = {"Temperature", "Humidity", "UV Intensity", "Light FLux", "Atmospheric Pressure", "Altitude"};
+	Description.units = {"C", "%RH", "mw/cm^2", "lx", "kpa", "m"};
+	Description.id = 1;
+	return env_sensor.begin() == 0;
+}
+
+/// @brief Takes a measurement
+/// @return True on suceess
+bool DFMultiEnvironmental::takeMeasurement() {
+	values[0] = env_sensor.getTemperature(TEMP_C);
+	values[1] = env_sensor.getHumidity();
+	values[2] = env_sensor.getUltravioletIntensity();
+	values[3] = env_sensor.getLuminousIntensity();
+	values[4] = env_sensor.getAtmospherePressure(KPA);
+	values[5] = env_sensor.getElevation();
+	return true;
+}
---
+++ src/DFMultiEnvironmental.h
@@ -0,0 +1,24 @@
+/*
+ * This file and associated .cpp file are licensed under the GPLv3 License Copyright (c) 2024 Sam Groveman
+ * 
+ * External libraries needed:
+ * DFRobot_EnvironmentalSensor: https://github.com/DFRobot/DFRobot_EnvironmentalSensor
+ * DFRobot_RTU: https://github.com/DFRobot/DFRobot_RTU
+ * 
+ * Contributors: Sam Groveman
+ */
+
+#pragma once
+#include <Sensor.h>
+#include <Wire.h>
+#include <DFRobot_EnvironmentalSensor.h>
+
+class DFMultiEnvironmental : public Sensor {
+	public:
+		DFMultiEnvironmental(TwoWire* I2C_bus = &Wire, uint8_t address = SEN050X_DEFAULT_DEVICE_ADDRESS);
+		bool begin();
+		bool takeMeasurement();
+		
+	private:
+		DFRobot_EnvironmentalSensor env_sensor;
+};
