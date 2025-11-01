86d0b13f5e60690b7e0a9dc54cf0096a31167dea
Sam Groveman <samgroveman@outlook.com>
2025-11-01 11:37:40 -0400

    Added ability to set I2C pins

--- src/DFMultiEnvironmental.cpp
+++ src/DFMultiEnvironmental.cpp
@@ -4,7 +4,21 @@
 /// @param Name The device name
 /// @param I2C_bus The I2C bus attached to the sensor
 /// @param address Address of the sensor
-DFMultiEnvironmental::DFMultiEnvironmental(String Name, TwoWire* I2C_bus, uint8_t address) : env_sensor(address, I2C_bus), Sensor(Name) {}
+DFMultiEnvironmental::DFMultiEnvironmental(String Name, TwoWire* I2C_bus, uint8_t address) : env_sensor(address, I2C_bus), Sensor(Name) {
+	i2c_bus = I2C_bus;
+}
+
+/// @brief Creates a new multi environmental sensor object
+/// @param Name The device name
+/// @param I2C_bus The I2C bus attached to the sensor
+/// @param address Address of the sensor
+/// @param sda SDA pin to use for I2C bus
+/// @param scl SCL pin to use for I2C bus
+DFMultiEnvironmental::DFMultiEnvironmental(String Name, int sda, int scl, TwoWire* I2C_bus, uint8_t address) : env_sensor(address, I2C_bus), Sensor(Name) {
+	i2c_bus = I2C_bus;
+	scl_pin = scl;
+	sda_pin = sda;
+}
 
 /// @brief Starts the environmental sensor
 /// @return True on success
@@ -14,6 +28,17 @@ bool DFMultiEnvironmental::begin() {
 	Description.parameters = {"Temperature", "Humidity", "UV Intensity", "Light Flux", "Atmospheric Pressure", "Altitude"};
 	Description.units = {"C", "%RH", "mw/cm^2", "lx", "kpa", "m"};
 	values.resize(Description.parameterQuantity);
+
+	// Start I2C bus if not started
+	if (scl_pin > -1 && sda_pin > -1) {
+		if (!i2c_bus->begin(sda_pin, scl_pin)) {
+			return false;
+		}
+	} else {
+		if (!i2c_bus->begin()) {
+			return false;
+		}
+	}
 	return env_sensor.begin() == 0;
 }
 
--- src/DFMultiEnvironmental.h
+++ src/DFMultiEnvironmental.h
@@ -16,9 +16,19 @@
 class DFMultiEnvironmental : public Sensor {
 	public:
 		DFMultiEnvironmental(String Name, TwoWire* I2C_bus = &Wire, uint8_t address = SEN050X_DEFAULT_DEVICE_ADDRESS);
+		DFMultiEnvironmental(String Name, int sda, int scl, TwoWire* I2C_bus = &Wire, uint8_t address = SEN050X_DEFAULT_DEVICE_ADDRESS);
 		bool begin();
 		bool takeMeasurement();
 		
 	protected:
+		/// @brief I2C bus in use
+		TwoWire* i2c_bus;
+
+		/// @brief SCL pin in use
+		int scl_pin = -1;
+
+		/// @brief SDA pin in use
+		int sda_pin = -1;
+
 		DFRobot_EnvironmentalSensor env_sensor;
 };
